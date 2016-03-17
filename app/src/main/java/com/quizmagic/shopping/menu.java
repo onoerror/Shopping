22740d739e285b4885eab3dbb58e6a04292796ca
onoerror <a3575131c@gmail.com>
2016-03-17 11:27:51 +0800

    改善後

--- app/src/main/java/com/quizmagic/shopping/menu.java
+++ app/src/main/java/com/quizmagic/shopping/menu.java
@@ -14,7 +14,9 @@ public class menu extends AppCompatActivity {
 
     private int mQuantity;
     private int mPrice = 5;
-    private String Milktea = "奶茶";
+    private String mName = "小野";
+    private StringBuilder mPriceMessage = new StringBuilder("NT$"+mPrice);
+    private StringBuilder mQuantityMessage = new StringBuilder(mQuantity);
 
 
     @Override
@@ -27,29 +29,6 @@ public class menu extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
     }
 
-    public void increment(View view) {
-        displayQuantity();
-        ++mQuantity;
-        resetTotolPrice();
-
-    }
-
-    public void decrement(View view) {
-        if (mQuantity > 0) {
-            --mQuantity;
-            displayQuantity();
-            resetTotolPrice();
-        }
-    }
-
-    private void resetTotolPrice(){
-        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
-        priceTextView.setText("");
-    }
-    public void submitOrder(View view) {
-        displayTotalPrice();
-    }
-
     private void displayTotalPrice() {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         int total = mPrice * mQuantity;
@@ -59,16 +38,102 @@ public class menu extends AppCompatActivity {
         priceTextView.setText(message);
     }
 
-    private void displayQuantity() {
-        TextView quantityTextView = (TextView) findViewById(R.id.quavtity_text_view);
-        quantityTextView.setText(String.valueOf(mQuantity));
+
+
+
+    public void clickToppings(View view) {
+        resetPriceMessageString();
+        displayPriceMessage();
+    }
+
+    private void resetPriceMessageString(){
+        clearPriceMessageString();
+        mPriceMessage.append("奶茶")
+                .append("NT$").append(mPrice);
+    }
+
+    private void displayPriceMessage(){
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText(mPriceMessage);
+    }
+
+    private void displayQuantityMessage(){
+        TextView quantityTextView =
+                (TextView)findViewById(R.id.quavtity_text_view);
+        int start = 0;
+        int end = mQuantityMessage.length();
+        mQuantityMessage.delete(start,end).append(mQuantity);
+        quantityTextView.setText(mQuantityMessage);
     }
 
 
-    public void mk(View view) {
-        final CheckBox mk = (CheckBox)findViewById(R.id.checkBox);
-        if(mk.isChecked()){
+    public void increment(View view) {
+        //+的變數
+        if (mQuantity<100) {
+        //100以內才會作用，不會超過100
+            ++mQuantity;
+            //數字遞增
+            displayQuantityMessage();
+            //顯示遞增遞減的數字，有垃圾車會處理
             resetTotolPrice();
+            //點選+的按鈕清除下方顯示字串
         }
     }
+
+    public void decrement(View view) {
+        //-的變數
+        if (mQuantity > 0) {
+            //數字大於0才有作用，不會低於0
+            --mQuantity;
+            //數字遞減
+            displayQuantityMessage();
+            //顯示遞增遞減的數字，有垃圾車會處理
+            resetTotolPrice();
+            //點選-的按鈕清除下方顯示字串
+        }
+    }
+
+    private void resetTotolPrice(){
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText("");
+    }
+    public void submitOrder(View view) {
+        clearPriceMessageString();//刪除mPriceMessage
+        concatPriceMessageString();//字串組合 人名 + 漢堡 + 是否加奶茶 +Free 或 NT$xxx Thank you!
+        displayPriceMessage();//displayPriceMessage()
+
+    }
+
+    private void clearPriceMessageString(){
+        int start = 0;
+        int end = mPriceMessage.length();
+        mPriceMessage.delete(start, end);
+    }
+
+    private void concatPriceMessageString(){
+        CheckBox checkBox = (CheckBox)findViewById(R.id.toppings_checkbox);
+        mPriceMessage.append("Name: ")
+                .append(mName)
+                .append("\n")
+                .append("加奶茶?")
+                .append(checkBox.isChecked())
+                .append("\n");
+        if(mQuantity == 0){
+            mPriceMessage.append("Free");
+        }else{
+            mPriceMessage.append("Quantity:")
+                    .append(mQuantity)
+                    .append("\n")
+                    .append("Total: ")
+                    .append("NT$")
+                    .append(mPrice * mQuantity)
+                    .append("\n")
+                    .append("Thank you!")
+                    .append("\n");
+        }
+    }
+
+
+
+
 }
